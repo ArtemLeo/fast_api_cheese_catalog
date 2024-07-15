328cd0cb765bc5a3b5ed72a8c8051dd6939d77d3
Artem Andriushchenko <link.by.artem@gmail.com>
2024-07-15 23:03:17 +0300

    Added models

---
+++ db/models.py
@@ -0,0 +1,30 @@
+from enum import StrEnum, auto
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+from db.engine import Base
+
+
+class PackagingType(StrEnum):
+    IN_PACKAGING = auto()
+    WEIGHT = auto()
+
+
+class DBCheeseType(Base):
+    __tablename__ = "cheese_type"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(String(551), nullable=False)
+
+
+class DBCheese(Base):
+    __tablename__ = "cheese"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    price = Column(Integer, nullable=False)
+    packaging_type = Column(Enum(PackagingType), nullable=False)
+    cheese_type_id = Column(Integer, ForeignKey("cheese_type.id"))
+
+    cheese_type = relationship(DBCheeseType)
